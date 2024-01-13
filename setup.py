bc7d296abaaad674567faa3ef127dc8f1e19ca0b
MILAN BHAKTA <milzbhakta@gmail.com>
2024-01-13 17:04:38 -0500

    change3

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='pythonProject',
     version='0.1.0',
     author='MILAN BHAKTA',
-    packages=['pythonProject'],
+    packages=find_packages(),
 )
 
